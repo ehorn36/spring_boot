18edcc382fd069b10c5318899cf3fcf1ea2ebd46
ehorn36 <ehorn36@gmail.com>
2026-01-31 09:22:42 -0500

    Add additional controllers

--- src/main/java/io/javabrains/springbootstarter/hello/HelloController.java
+++ src/main/java/io/javabrains/springbootstarter/hello/HelloController.java
@@ -3,10 +3,20 @@ package io.javabrains.springbootstarter.hello;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+/*
+In Spring Boot (and Spring MVC), a Controller is a Java class responsible for 
+handling incoming HTTP requests from the outside world. It acts as the entry point for your application logic. 
+
+In Spring Boot, @RestController is a specific type of controller annotation used to build RESTful web services.
+*/
 @RestController
 public class HelloController {
 
-    // Default mapping == GET request
+    /*
+    In Spring Boot, @RequestMapping is the annotation used to map web requests (URLs) 
+    to specific classes and methods in your code. It acts as the Router.
+     */
     @RequestMapping("/hello")
     public String sayHi() {
         return "Hi!";
---
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -0,0 +1,28 @@
+package io.javabrains.springbootstarter.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+
+    // @RequestMapping("/topics")
+    // public String getAllTopics() {
+    //     return "All Topics";
+    // }
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics() {
+        return Arrays.asList(
+            new Topic(),
+            new Topic(),
+            new Topic(),
+            new Topic("spring", "Spring Framework", "Spring Framework Description")
+        );
+    }
+
+
+}
